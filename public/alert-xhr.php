cb4009a10360f1846a6e07bb12c7afc4a7fef67e
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-06-22 21:26:11 +0400

    update

--- public/alert-xhr.php
+++ public/alert-xhr.php
@@ -8,70 +8,70 @@ header('Content-Type: application/json');
 
 $status = $ovh->get('/status/task');
 
-$result = array();
+$result = [];
 
 foreach ($status as $s) {
-  if (isset($_GET['cloud']) && $s['project'] !== 'PublicCloud') {
-    continue;
-  }
-  if (isset($_GET['vps']) && $s['project'] !== 'VPS') {
-    continue;
-  }
-
-  if ($s['project'] === 'PublicCloud') {
-    if (!isset($result[$s['impactedService']])) {
-      $result[$s['impactedService']] = array();
+    if (isset($_GET['cloud']) && $s['project'] !== 'PublicCloud') {
+        continue;
     }
-    if (!isset($result[$s['impactedService']][$s['uuid']])) {
-      $result[$s['impactedService']][$s['uuid']] = array(
-        'status' => NULL,
-        'alerts' => array()
-      );
+    if (isset($_GET['vps']) && $s['project'] !== 'VPS') {
+        continue;
     }
 
-    $result[$s['impactedService']][$s['uuid']]['alerts'][] = $s;
-
-    switch ($s['status']) {
-      case 'planned':
-        if ($result[$s['impactedService']][$s['uuid']]['status'] !== 'inProgress') {
-          $result[$s['impactedService']][$s['uuid']]['status'] = 'planned';
+    if ($s['project'] === 'PublicCloud') {
+        if (!isset($result[$s['impactedService']])) {
+            $result[$s['impactedService']] = [];
         }
-        break;
-      case 'inProgress':
-        $result[$s['impactedService']][$s['uuid']]['status'] = 'inProgress';
-        break;
-      case 'finished':
-        if ($result[$s['impactedService']][$s['uuid']]['status'] !== 'inProgress' && $result[$s['impactedService']][$s['uuid']]['status'] !== 'planned') {
-          $result[$s['impactedService']][$s['uuid']]['status'] = 'finished';
+        if (!isset($result[$s['impactedService']][$s['uuid']])) {
+            $result[$s['impactedService']][$s['uuid']] = [
+                'status' => null,
+                'alerts' => [],
+            ];
         }
-        break;
-    }
-  } else {
-    if (!isset($result[$s['impactedService']])) {
-      $result[$s['impactedService']] = array(
-        'status' => NULL,
-        'alerts' => array()
-      );
-    }
 
-    $result[$s['impactedService']]['alerts'][] = $s;
+        $result[$s['impactedService']][$s['uuid']]['alerts'][] = $s;
 
-    switch ($s['status']) {
-      case 'planned':
-        if ($result[$s['impactedService']]['status'] !== 'inProgress') {
-          $result[$s['impactedService']]['status'] = 'planned';
+        switch ($s['status']) {
+            case 'planned':
+                if ($result[$s['impactedService']][$s['uuid']]['status'] !== 'inProgress') {
+                    $result[$s['impactedService']][$s['uuid']]['status'] = 'planned';
+                }
+                break;
+            case 'inProgress':
+                $result[$s['impactedService']][$s['uuid']]['status'] = 'inProgress';
+                break;
+            case 'finished':
+                if ($result[$s['impactedService']][$s['uuid']]['status'] !== 'inProgress' && $result[$s['impactedService']][$s['uuid']]['status'] !== 'planned') {
+                    $result[$s['impactedService']][$s['uuid']]['status'] = 'finished';
+                }
+                break;
         }
-        break;
-      case 'inProgress':
-        $result[$s['impactedService']]['status'] = 'inProgress';
-        break;
-      case 'finished':
-        if ($result[$s['impactedService']]['status'] !== 'inProgress' && $result[$s['impactedService']]['status'] !== 'planned') {
-          $result[$s['impactedService']]['status'] = 'finished';
+    } else {
+        if (!isset($result[$s['impactedService']])) {
+            $result[$s['impactedService']] = [
+                'status' => null,
+                'alerts' => [],
+            ];
         }
-        break;
+
+        $result[$s['impactedService']]['alerts'][] = $s;
+
+        switch ($s['status']) {
+            case 'planned':
+                if ($result[$s['impactedService']]['status'] !== 'inProgress') {
+                    $result[$s['impactedService']]['status'] = 'planned';
+                }
+                break;
+            case 'inProgress':
+                $result[$s['impactedService']]['status'] = 'inProgress';
+                break;
+            case 'finished':
+                if ($result[$s['impactedService']]['status'] !== 'inProgress' && $result[$s['impactedService']]['status'] !== 'planned') {
+                    $result[$s['impactedService']]['status'] = 'finished';
+                }
+                break;
+        }
     }
-  }
 }
 
 echo json_encode($result);
